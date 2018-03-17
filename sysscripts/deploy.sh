e563e9e05a88afaf36f80b5c4dbfde87ec4e377e
Esteban Goya <emgoya@gmail.com>
2018-03-17 13:06:47 +0000

    mailing

--- sysscripts/deploy.sh
+++ sysscripts/deploy.sh
@@ -2,7 +2,9 @@
 git pull origin master &&
 bin/console ca:cl --env=dev &&
 bin/console ca:cl --env=prod &&
+bin/console cache:warmup --env=prod &&
 chmod -R 777 var/cache &&
 chmod -R 777 var/logs &&
 chmod -R 777 web/uploads/ &&
+chmod -R 777 web/media &&
 sh install_front_deps.sh
