78e04978766d0f7a1db90780d9dae114ccad4b5f
Esteban Goya <emgoya@gmail.com>
2021-11-02 10:30:55 +0000

    Regen images command

---
+++ src/MotogpBundle/Command/RegenImagesCommand.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace MotogpBundle\Command;
+
+use Application\Sonata\MediaBundle\Entity\FeaturedMedia;
+use Application\Sonata\MediaBundle\Entity\FooterImage;
+use Application\Sonata\MediaBundle\Entity\GalleryMedia;
+use Application\Sonata\MediaBundle\Entity\HeaderImage;
+use Application\Sonata\MediaBundle\Entity\HomeImage;
+use Application\Sonata\MediaBundle\Entity\Logo;
+use Application\Sonata\MediaBundle\Entity\MediaImage;
+use Application\Sonata\MediaBundle\Entity\NewsLetterMedia;
+use Application\Sonata\MediaBundle\Entity\PostMedia;
+use Application\Sonata\MediaBundle\Entity\PreviewImage;
+use Application\Sonata\MediaBundle\Entity\QuotationImage;
+use Application\Sonata\MediaBundle\Entity\RiderMedia;
+use Application\Sonata\MediaBundle\Entity\TeamStaffImage;
+use Application\Sonata\MediaBundle\Entity\Media;
+use MotogpBundle\Entity\Gallery;
+use Psr\Log\LoggerInterface;
+use stringEncode\Exception;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use MotogpBundle\Services\Newsletters;
+use MotogpBundle\Entity\Newsletter;
+
+
+class RegenImagesCommand extends ContainerAwareCommand
+{
+
+    //TODO: get available locales from app config
+    const LOCALE_EN = 1;
+    const LOCALE_ES = 2;
+
+
+
+    protected function configure()
+    {
+        $this
+            ->setName('motogp:regen:images')
+            ->setDescription('regen-images')
+            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function updateCache () {
+//        $cacheManager = $this->getContainer()->get('liip_imagine.cache.manager');
+//        $cacheManager->remove();
+       // $cacheManager->resolve();
+    }
+    
+    protected function regenImages() {
+
+       $images = [];
+
+
+       $images = array_merge($images, $this->em->getRepository(FeaturedMedia::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(FooterImage::class)->findAll());
+//       $images = array_merge($images, $this->em->getRepository(Gallery::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(GalleryMedia::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(HeaderImage::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(HomeImage::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(Logo::class)->findAll());
+//       $images = array_merge($images, $this->em->getRepository(Media::class)->findAll());
+//       $images = array_merge($images, $this->em->getRepository(MediaImage::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(NewsLetterMedia::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(PostMedia::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(PreviewImage::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(QuotationImage::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(RiderMedia::class)->findAll());
+       $images = array_merge($images, $this->em->getRepository(TeamStaffImage::class)->findAll());
+
+       foreach ($images as $f) {
+
+           if ($f->getProviderMetadata() && $f->getProviderMetadata()['filename']) {
+               $fsUrl = __DIR__ . '/../../../web/uploads/' . $f->getProviderMetadata()['filename'];
+               if (file_exists($fsUrl)){
+                    $f->setBinaryContent($fsUrl);
+                }
+           }
+
+       }
+
+
+       $this->em->flush();
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->em = $this->getContainer()->get('doctrine')->getManager();
+        $this->output = $output;
+
+        $this->regenImages();
+    }
+
+}
